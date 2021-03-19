521b2aba6ae6e4d9cabdd7b36dd43b3cdd985454
sksketer <amrit.agarwal_cs18@gla.ac.in>
2021-03-19 11:16:07 +0530

    Create Footer.js

---
+++ src/component/Footer.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react';
+import '../css/footer.css';
+
+const Footer = () => {
+    return ( <div className="footer">
+                <p>@copyright 2021</p>
+                <div className="follow">
+
+                </div>
+            </div> )
+
+}
+
+export default Footer;

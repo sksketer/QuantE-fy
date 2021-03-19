c8179d1a71418acdc679ef7383d6d7f1630672b1
sksketer <amrit.agarwal_cs18@gla.ac.in>
2021-03-19 11:15:35 +0530

    Create RightBar.js

---
+++ src/component/RightBar.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react';
+import '../css/right-bar.css';
+
+const RightBar = () => {
+    return <div className="right-bar">
+            <h4>SubTopics</h4>
+
+
+            </div>
+
+}
+
+export default RightBar;

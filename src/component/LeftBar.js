46f48ce647691cdc5ce18f194bced01e3e4d165a
sksketer <amrit.agarwal_cs18@gla.ac.in>
2021-03-19 11:15:59 +0530

    Create LeftBar.js

---
+++ src/component/LeftBar.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react';
+import '../css/left-bar.css';
+
+const LeftBar = () => {
+    return <div className="left-bar">
+            <h4>Topics</h4>
+
+
+            </div>
+
+}
+
+export default LeftBar;

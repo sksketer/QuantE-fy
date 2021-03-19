03e6dbee95fa56512d1e31d8277fd0c076bc8712
sksketer <amrit.agarwal_cs18@gla.ac.in>
2021-03-20 00:24:02 +0530

    Update RightBar.js

--- src/component/RightBar.js
+++ src/component/RightBar.js
@@ -5,6 +5,11 @@ import '../css/right-bar.css';
 const RightBar = () => {
     return <div className="right-bar">
             <h4>SubTopics</h4>
+            <h6><a href="#">1</a></h6>
+            <h6><a href="#">2</a></h6>
+            <h6><a href="#">3</a></h6>
+            <h6><a href="#">4</a></h6>
+            <h6><a href="#"></a></h6>
 
 
             </div>

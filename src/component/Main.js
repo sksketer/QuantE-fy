f7e0c16873826dc2220d963d30c317dcdd3aaaa2
sksketer <amrit.agarwal_cs18@gla.ac.in>
2021-03-19 11:15:47 +0530

    Create Main.js

---
+++ src/component/Main.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react';
+import '../css/main.css';
+
+const Main = () => {
+    return <div className="main-container">
+            <h4>Heading</h4>
+
+
+            </div>
+
+}
+
+export default Main;

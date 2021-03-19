bec988ce46f5f34cfb06449418947a86b809c373
sksketer <amrit.agarwal_cs18@gla.ac.in>
2021-03-20 00:23:59 +0530

    Update Main.js

--- src/component/Main.js
+++ src/component/Main.js
@@ -1,13 +1,18 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react';
 import '../css/main.css';
+import NumberSystem from './Topics/NumberSystem';
 
 const Main = () => {
-    return <div className="main-container">
-            <h4>Heading</h4>
+    return (
+        <div className="main-container">
+            {/* <h4>Welcome to QuantE-fy</h4>
+            <p>All Quant and Reasning questions Best solution are here.</p> */}
+            <NumberSystem />
 
 
-            </div>
+        </div>
+    )
 
 }
 

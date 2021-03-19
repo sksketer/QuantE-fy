6d46e6c2dc4c045d9abc114682562c6af69bcbc4
sksketer <amrit.agarwal_cs18@gla.ac.in>
2021-03-19 11:13:07 +0530

    add navigation bar to web page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import NavBar from './component/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
+      <LeftBar />
+      <Main />
+      <RightBar />
+      <Footer />
     </div>
   );
 }

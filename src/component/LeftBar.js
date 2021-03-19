9d88ad63215adc8946bdb4de1299a238b188f838
sksketer <amrit.agarwal_cs18@gla.ac.in>
2021-03-20 00:23:55 +0530

    Update LeftBar.js

--- src/component/LeftBar.js
+++ src/component/LeftBar.js
@@ -5,6 +5,9 @@ import '../css/left-bar.css';
 const LeftBar = () => {
     return <div className="left-bar">
             <h4>Topics</h4>
+            <h6><a href="#">Number System</a></h6>
+            <h6><a href="#">Divisibility</a></h6>
+            <h6><a href="#">Factors and multiples</a></h6>
 
 
             </div>

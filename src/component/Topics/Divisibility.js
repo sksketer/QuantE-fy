6c9dea7d82af0a97b42b9a6f114b1f7b571ebf5e
sksketer <amrit.agarwal_cs18@gla.ac.in>
2021-03-20 15:21:02 +0530

    Create Divisibility.js

---
+++ src/component/Topics/Divisibility.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+
+const Divisibility = () => {
+    return (
+        <div className="divisibility">
+            <h2>Divisibility</h2>
+        </div>
+
+    )
+}
+
+export default Divisibility;

be091c8c1f74986e390ce228c19fb24bcb8a731e
sksketer <amrit.agarwal_cs18@gla.ac.in>
2021-03-20 15:20:59 +0530

    Create Default.js

---
+++ src/component/Topics/Default.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+
+const Default = () => {
+    return (
+        <div className="default">
+            <h4>Welcome to QuantE-fy</h4>
+            <p>All Quant and Reasning questions Best solution are here.</p>
+        </div>
+    )
+}
+
+export default Default;
+            

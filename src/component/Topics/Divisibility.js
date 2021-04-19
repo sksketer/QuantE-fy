112775880e8e0498f7c10638cf79ac289c66369b
sksketer <amrit.agarwal_cs18@gla.ac.in>
2021-04-19 09:28:14 +0530

    Update Divisibility.js

--- src/component/Topics/Divisibility.js
+++ src/component/Topics/Divisibility.js
@@ -1,8 +1,9 @@
 import React,{Component} from 'react';
+import '../../css/Topics/topic.css';
 
 const Divisibility = () => {
     return (
-        <div className="divisibility">
+        <div className="main-heading">
             <h2>Divisibility</h2>
         </div>
 

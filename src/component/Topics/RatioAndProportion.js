88dac0f2412c62240d43999cadf9cd0ec7ba95ba
sksketer <amrit.agarwal_cs18@gla.ac.in>
2021-04-19 09:28:33 +0530

    Create RatioAndProportion.js

---
+++ src/component/Topics/RatioAndProportion.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../../css/Topics/topic.css';
+
+const RatioAndProportion = () => {
+    return (
+            <div className="main-heading">
+                <h2>Ratio and Proportion</h2>
+                <h4><p>Quick Revision</p></h4>
+                <div className="tips">
+                    <p className="def"><b>1. To find out the proportation number</b></p>
+                    <p className="def-mg">a:b::c:d <br></br>first proportational = a<br></br> second proportational = b <br></br></p>
+
+                </div>
+            </div>
+
+    )
+}
+
+export default RatioAndProportion;

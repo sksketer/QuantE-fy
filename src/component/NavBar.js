88c0f6d829706cd49cf4898fe3361c6090b62d55
sksketer <amrit.agarwal_cs18@gla.ac.in>
2021-04-19 09:28:08 +0530

    Update NavBar.js

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -4,12 +4,12 @@ import "../css/nav.css";
 
 const NavBar = () => {
     return <div className="nav-bar">
-                <div className="logo"> <img src="" alt="Image Not Found"></img> </div>
+                <div className="logo"> <img src="" alt="L7L7L7L7L7L7L7"></img> </div>
                 <div className="links"> 
                     <ul>
                         <li><a href="#">Home</a></li>
-                        <li><a href="#">Quiz</a></li>
-                        <li><button>Sign-in</button></li>
+                        {/* <li><a href="#">Quiz</a></li> */}
+                        <li><a href="http://localhost/QuantE-FY/src/php/ask.html" target="_blank">Quiz</a></li>
                     </ul>
                 </div>
             </div>

633cbc673672ad17d54e7df62088cdbab67c31c7
sksketer <amrit.agarwal_cs18@gla.ac.in>
2021-04-19 09:27:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,19 +4,30 @@ import RightBar from './component/RightBar';
 import LeftBar from './component/LeftBar';
 import Main from './component/Main';
 import Footer from './component/Footer';
+import {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <NavBar />
-      <div className="main">
-        <LeftBar />
-        <Main />
-        <RightBar />
+class App extends Component {
+  state = {
+    componentToRender: ""
+  }
+  
+  myfun = (event) => {
+    this.setState({ componentToRender: event.target.textContent});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <NavBar />
+        <div className="main">
+          <LeftBar myfun={this.myfun}/>
+          <Main componentToRender={this.state.componentToRender} />
+          {/* <RightBar /> */}
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
-  );
+    );
+  }
 }
 
 export default App;

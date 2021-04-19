ad2cbd30f306f58cc08e8c2757c521a853a01f44
sksketer <amrit.agarwal_cs18@gla.ac.in>
2021-04-19 09:29:10 +0530

    Create ask2.php

---
+++ src/php/ask2.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/ask.css">
+    <title>QuantE-FY</title>
+    <style>        
+        .ask .logout {
+            position: absolute;
+            right: 15px;
+            top: 10px;
+            display: inline;
+        }
+        .ask .logout:hover {
+            border-bottom: 1px solid red;
+        }
+        .ask .logout a {
+            color: red;
+            text-decoration: none;
+        }
+        .ask{
+            position: relative;
+        }
+    </style>
+</head>
+<body>
+    <?php
+        session_start();
+        $fname = $_SESSION['fname'];
+        $lname = $_SESSION['lname'];
+        $email = $_SESSION['email'];
+    ?>
+    <div class="ask">
+        <div class="logout"><a href="http://localhost:3000/">Log-out</a></div>
+        <h2>Hello, <?php echo $fname; ?></h2>
+        <p>
+            <a href="http://localhost/QuantE-FY/src/php/user/quiz.php">Attemp Quiz</a>
+        </p> 
+        <p>
+            <a href="http://localhost/QuantE-FY/src/php/user/dout.php">Query in any topics.</a>
+        </p>
+    </div>
+</body>
+</html>

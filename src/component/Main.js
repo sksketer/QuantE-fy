e1f1cf666cb439aa51d2d315fe298eae735b454e
sksketer <amrit.agarwal_cs18@gla.ac.in>
2021-04-25 00:33:27 +0530

    Update Main.js

--- src/component/Main.js
+++ src/component/Main.js
@@ -5,6 +5,8 @@ import NumberSystem from './Topics/NumberSystem';
 import Divisibility from './Topics/Divisibility';
 import Default from './Topics/Default';
 import RatioAndProportion from './Topics/RatioAndProportion';
+import ProfitAndLos from './Topics/profitAndLos';
+import SimpleInterest from './Topics/SimpleInterest';
 
 const Main = ({componentToRender}) => {
 
@@ -13,7 +15,9 @@ const Main = ({componentToRender}) => {
     if(page == "") show = <Default />;
     else if(page == "Number System") show = <NumberSystem />;
     else if(page == "Divisibility") show = <Divisibility />;
-    else if(page == "RatioAndProportion") show = <RatioAndProportion />;
+    else if(page == "Ratio and Proportation") show = <RatioAndProportion />;
+    else if(page == "Profit And Loss") show = <ProfitAndLos />;
+    else if(page == "Simple Interest") show = <SimpleInterest />;
 
     return (
         <div className="main-container">

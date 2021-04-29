9c3c17ef4d552baf03ffd4d9a39dec794e0d8910
sksketer <amrit.agarwal_cs18@gla.ac.in>
2021-04-29 13:43:48 +0530

    Update quiz.php

--- src/php/user/quiz.php
+++ src/php/user/quiz.php
@@ -1,3 +1,4 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,7 +9,7 @@
     <title>Quiz</title>
 </head>
 <body>
-<div class="container">
+    <div class="container">
         <form action="" method="post">
             <h2><span>Quiz</span></h2>
             <?php
@@ -24,40 +25,56 @@
                 die("Connection failed: " . $conn->connect_error);
                 }
 
-                $sql = "SELECT sno, question, option1, option2, option3, option4 FROM `quiz-question`";
+                $sql = "SELECT sno, question, option1, option2, option3, option4, answer FROM `quiz-question`";
                 $result=mysqli_query($conn,$sql); 
 
                 if (mysqli_num_rows ( $result ) > 0) {
                 // output data of each row
+                    $i = 1;
                     while($row=mysqli_fetch_assoc($result)) {
-                        $i = 1;
+                        $questionumber = "q".$i;
+
                         echo '<div class="q">';
-                        echo '<label for="">Q- '. $row["question"] .'</label> <br><br>';
-                        echo '<input type="radio" name="q1" id="">'. $row["option1"] .'<br>';
-                        echo '<input type="radio" name="q1" id="">'. $row["option2"] .'<br>';
-                        echo '<input type="radio" name="q1" id="">'. $row["option3"] .'<br>';
-                        echo '<input type="radio" name="q1" id="">'. $row["option4"] .'<br>';
-                        // $correctAnswer = $row["answer"];
+                        echo '<label for="">Q-'.$i++.' '. $row["question"] .'</label> <br><br>';
+                        echo '<input type="radio" name="'.$questionumber.'" id="" value="1"> '. $row["option1"] .'<br>';
+                        echo '<input type="radio" name="'.$questionumber.'" id="" value="2"> '. $row["option2"] .'<br>';
+                        echo '<input type="radio" name="'.$questionumber.'" id="" value="3"> '. $row["option3"] .'<br>';
+                        echo '<input type="radio" name="'.$questionumber.'" id="" value="4"> '. $row["option4"] .'<br>';
+                        $correctAnswer[$row["sno"]] = $row["answer"];
                         echo"</div>"."<br>";
-                        $i++;
                     }
                 } else {
-                echo "0 results";
+                    echo "0 results";
                 }
-                $conn->close();
             ?> 
-            <!-- <div class="q">
-                <label for="">Q1- Factorial of 5 is</label> <br>
-                <input type="radio" name="q1" id=""> 120 <br>
-                <input type="radio" name="q1" id=""> 150 <br>
-                <input type="radio" name="q1" id=""> 25 <br>
-                <input type="radio" name="q1" id=""> none of above <br>
-            </div> <br> -->
+            <input type="hidden" name="btn" value="1" id="">
+            <input type="submit" value="save">
         </form>
         <div class="detail">
             <img src="../../img/photo.jpg" alt="Image Not Found" width="100px" height="100px">
-            <p>Name</p>
+            <p><?php echo $_SESSION['fname']." ".$_SESSION['lname']; ?></p>
         </div>
+    </div>
+    <div class="result">
+        <?php
+            echo "Calculating score"."<br>";
+        ?>
+        <?php
+            sleep(5);
+            $btn = "";
+            $btn = $_POST['btn'];
+            if($btn != "") {
+                $score = 0;
+                $numberOfRows = mysqli_num_rows($result);
+                for($i = 1; $i <= $numberOfRows; $i++) {
+                    $q = 'q'.$i;
+                    $useranswer[$i] = $_POST[$q];
+                    if($useranswer[$i] == $correctAnswer[$i])
+                        $score++;
+                }
+                echo "Correct ".$score ." out of ".$numberOfRows;
+            }
+        ?>
     </div>
 </body>
 </html>

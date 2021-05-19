d2382e797f73ac857ff6d1946e4266c3b89b394b
sksketer <amrit.agarwal_cs18@gla.ac.in>
2021-05-19 20:41:40 +0530

    Update LeftBar.js

--- src/component/LeftBar.js
+++ src/component/LeftBar.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react';
 import '../css/left-bar.css';
 
 const LeftBar = (props) => {
-    const buttons = ["Number System", "Divisibility", "Factors and multiples", "Ratio and Proportation", "Simple Interest", "Profit And Loss"];
+    const buttons = ["Number System", "Divisibility", "Factors and multiples", "Ratio and Proportation", "Simple Interest", "Percentage", "Divisibility", "Average", "Ratio And Proportion","Mixture And Alligations"];
     const topics = buttons.map((buttonName, index) => {
         return (
             <h6 key={index}><button onClick={props.myfun}>{buttonName}</button></h6>

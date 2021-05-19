c16fcf35d7f7d69277f73c407c610ad01e76ce96
sksketer <amrit.agarwal_cs18@gla.ac.in>
2021-05-19 20:42:08 +0530

    Update RatioAndProportion.js

--- src/component/Topics/RatioAndProportion.js
+++ src/component/Topics/RatioAndProportion.js
@@ -1,17 +1,66 @@
-import React from 'react';
+import React,{Component} from 'react';
 import '../../css/Topics/topic.css';
 
 const RatioAndProportion = () => {
     return (
-            <div className="main-heading">
-                <h2>Ratio and Proportion</h2>
-                <h4><p>Quick Revision</p></h4>
-                <div className="tips">
-                    <p className="def"><b>1. To find out the proportation number</b></p>
-                    <p className="def-mg">a:b::c:d <br></br>first proportational = a<br></br> second proportational = b <br></br></p>
+        <div className="main-heading">      
+        <h1>Ratio and Proportion</h1><br></br>
+        <h2><p><u>Quick Revision</u></p></h2><br></br>
+        <div className="tips">
+            <p className="def"><b>Ratio:</b>In certain situations, the comparison of two quantities by the method of division is very efficient. We can say that the comparison or simplified form of two quantities of the same kind is referred to as ratio </p>
+            <p className="def"><b>The two numbers in a ratio can only be compared when they have the same unit. We make use of ratios to compare two things.</b> </p>
+            <p className="def"><b>The sign used to denote a ratio is ‘:’.</b> </p>
+            <p className="def"><b>Proportion: </b> Proportion is an equation which defines that the two given ratios are equivalent to each other. In other words, the proportion states the equality of the two fractions or the ratios.</p>
+            <p className="def">When two ratios are equal in value, then they are said to be in <b>proportion</b>. </p>
+            <p className="def"><b></b> </p>
+            <p className="def"><b></b> </p><br></br>
+            
+            <h2><p><u> Questions for practise</u></p><br></br></h2> 
+            
+            <p className="def"><b> Q.1</b><b></b><br></br> <br></br>
+            <b>Sol.</b> </p><br></br>
 
-                </div>
-            </div>
+            <p className="def"><b> Q.2</b> <b></b> <br></br><br></br> 
+            <b>Sol.</b></p><br></br>
+            
+            <p className="def"><b> Q.3</b> <b></b> <br></br><br></br> 
+            <b>Sol.</b></p><br></br>
+
+            <p className="def"><b> Q.4</b> <b></b> <br></br><br></br> 
+            <b>Sol.</b></p><br></br>
+
+            <p className="def"><b> Q.5</b> <b></b> <br></br><br></br> 
+            <b>Sol.</b></p><br></br>
+
+            <p className="def"><b> Q.6</b> <b></b> <br></br><br></br> 
+            <b>Sol.</b></p><br></br>
+
+            <p className="def"><b> Q.7</b> <b></b> <br></br><br></br> 
+            <b>Sol.</b></p><br></br>
+
+            <p className="def"><b> Q.8</b> <b></b> <br></br><br></br> 
+            <b>Sol.</b></p><br></br>
+
+            <p className="def"><b> Q.9</b> <b></b> <br></br><br></br> 
+            <b>Sol.</b></p><br></br>  
+
+            <p className="def"><b> Q.10</b> <b></b> <br></br><br></br> 
+            <b>Sol.</b></p><br></br>  
+
+            <h2><p><u> More Questions</u></p><br></br></h2>
+            
+            <p className="def"><b> Q.1</b><b></b>  </p><br></br>
+
+            <p className="def"><b> Q.2</b><b></b>  </p><br></br>
+
+            <p className="def"><b> Q.3</b><b></b>  </p><br></br>
+
+            <p className="def"><b> Q.4</b><b></b>  </p><br></br>
+
+            <p className="def"><b> Q.5</b><b></b>  </p><br></br>
+            
+        </div>
+</div>
 
     )
 }

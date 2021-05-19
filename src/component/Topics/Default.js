249914fff3f37f738abb4592c9b7ec86b9c06438
sksketer <amrit.agarwal_cs18@gla.ac.in>
2021-05-19 20:41:52 +0530

    Update Default.js

--- src/component/Topics/Default.js
+++ src/component/Topics/Default.js
@@ -4,7 +4,7 @@ const Default = () => {
     return (
         <div className="default">
             <h4>Welcome to QuantE-fy</h4>
-            <p>All Quant and Reasoning questions Best solution are here.</p>
+            <p>All Quant and Reasning questions Best solution are here.</p>
         </div>
     )
 }
